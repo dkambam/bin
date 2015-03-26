3ce01c3c255c2d02fbee1f6d3da596551e275c43
Dheeraj K Reddy <kdr.code@gmail.com>
2015-03-26 05:18:05 -0400

    add working program error-handling/

--- c/error-handling/main.c
+++ c/error-handling/main.c
@@ -1,1 +1,12 @@
-int main(){}
+// demo: using errno, perror, strerror
+
+#include <stdio.h>
+#include <errno.h>
+
+int main(){
+	FILE *fd = fopen("non-file", "r");
+	if( fd == NULL ){
+		perror( "open()" );
+		printf("strerror says, \"%s\"\n", strerror(errno));
+	}
+}
